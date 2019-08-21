a297e49a2dbff9b79af8dc02380f6e1a4f18ccb8
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-21 15:30:23 +0300

    added the classes registers.

--- life/admin.py
+++ life/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Profile,Housing,Business,Service
+from .models import Profile,Housing,Business,Services
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(OSMGeoAdmin):
-    list_display = ('_all_')
+    list_display = ('user','pic','bio')
     
 @admin.register(Housing)
 class HousingAdmin(OSMGeoAdmin):
-    list_display = ("_all_")
+    list_display = ('name','image','location','address','city','contact','description','verified')
     
 @admin.register(Business)
 class BusinessAdmin(OSMGeoAdmin):
-    list_display = ("_all_")
+    list_display = ('name','location','address','city','contact','description','category','verified')
     
-@admin.register(Service)
-class ServiceAdmin(OSMGeoAdmin):
-    list_display = ('_all_')
+@admin.register(Services)
+class ServicesAdmin(OSMGeoAdmin):
+    list_display = ('name','location','address','image','city','category','price','description','contact','available','verified')
