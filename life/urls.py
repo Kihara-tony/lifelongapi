cc7d25c087ca4199832722a62096d39083c41c8a
Tony Kihara <tonykiharatonkin@gmail.com>
2019-08-26 22:26:53 +0300

    added the url pattern of routers and then removed them.

--- life/urls.py
+++ life/urls.py
@@ -3,11 +3,15 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import ServicesViewSet,BusinessViewSet,HousingViewSet
+
 urlpatterns=[
     url('^$',views.housing,name = 'housing'),
     url('services/', ServicesViewSet.as_view({'get':'list'}), name="sevices-all"),
-    url('business/', BusinessViewSet.as_view({'get':'list'}), name="business-all"),
-    url('housing/', HousingViewSet.as_view({'get':'list'}), name="housing-all"),
+    url('services/<int:pk>/', ServicesViewSet.as_view({'put','list'}), name="sevices-all"),
+    url('business/<int:pk>/', BusinessViewSet.as_view({'get':'list'}), name="business-all"),
+    url('business/<int:pk>/', BusinessViewSet.as_view({'put':'list'}), name="business-all"),
+    url('housing/<int:pk>/', HousingViewSet.as_view({'get':'list'}), name="housing-all"),
+    url('housing/<int:pk>/', HousingViewSet.as_view({'put':'list'}), name="housing-all"),
     url(r'addhousing/',views.addhousing,name = 'addhousing'),
     url(r'^services/$', views.services, name='services'),
     url(r'addservices/',views.addservices,name = 'addservices'),
